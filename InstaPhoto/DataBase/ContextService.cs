4b399fd66d34ae2915ec84c818677a2f2af432e0
Carolina <carito_b10@hotmail.com>
2020-11-06 23:28:52 -0300

    Actualización de libs sqlite

---
+++ InstaPhoto/DataBase/ContextService.cs
@@ -0,0 +1,7 @@
+namespace DataBase
+{
+    public class ContextService
+    {
+        
+    }
+}
--- InstaPhoto/DataBase/Interfaces/IRepository.cs
+++ InstaPhoto/DataBase/Interfaces/IRepository.cs
@@ -1,7 +1,9 @@
+using Domain;
+
 namespace DataBase
 {
     public interface IRepository
     {
-        
+        void AddUser(User user);
     }
 }
