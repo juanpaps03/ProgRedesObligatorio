5cdee1a8fe99fef727387911b38e9064f5172cfd
Carolina <carito_b10@hotmail.com>
2020-11-30 23:44:16 -0300

    Create webapi and add users with postman

---
+++ InstaPhoto/WebApi/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("users")]
+    [ApiController]
+
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> SaveUserAsync(User user)
+        {
+            var responseUser = await _userService.SaveUserAsync(user);
+            return new CreatedResult(string.Empty, responseUser);
+        }
+        
+        
+    }
+}
